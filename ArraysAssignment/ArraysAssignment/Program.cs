47aa38f163d9c3b0ec6bb3d0da5df3904d019992
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2021-12-15 21:59:03 -0800

    Quick fixes
    Fixed code to return if users input is within range or not of index.

--- ArraysAssignment/ArraysAssignment/Program.cs
+++ ArraysAssignment/ArraysAssignment/Program.cs
@@ -11,30 +11,42 @@ namespace ArraysAssignment
             Console.WriteLine("Select an index of the array 0-4:");
             string[] favCars = {"Hyundai", "Honda", "Toyota", "Volvo", "Subaru"};
             int userInput = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(favCars[userInput]);
-
-            Console.WriteLine("Select an index of the array 0-4:");
-            int[] favNums = {3, 7, 12, 15, 22};
-            int yourInput = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(favNums[yourInput]);
-
-            if (yourInput == 0-4)
+            if (userInput < 0 || userInput > 4)
             {
-                Console.WriteLine(favNums);
+                Console.WriteLine("That index does not exist in this array.");
             }
             else
             {
-                Console.WriteLine("You are close");
+                Console.WriteLine(favCars[userInput]);
             }
 
+            Console.WriteLine("Select an index of the array 0-5:");
+            int[] favNums = {3, 7, 12, 15, 22, 27};
+            int yourInput = Convert.ToInt32(Console.ReadLine());
+            if (yourInput < 0 || yourInput > 5)
+            {
+                Console.WriteLine("That index is non existant.");
+            }
+            else
+            {
+                Console.WriteLine(favNums[yourInput]);
+            }
+
+            Console.WriteLine("Select an index of the array 0-3:");
             List<string> myList = new List<string>();
             myList.Add("Barry");
             myList.Add("Lothar");
             myList.Add("Aren");
             myList.Add("Cyrus");
             int myNames = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(myList[myNames]);
-            
+            if (myNames < 0 || myNames > 3)
+            {
+                Console.WriteLine("Index does not exist.");
+            }
+            else
+            {
+                Console.WriteLine(myList[myNames]);
+            }
             Console.ReadLine();
         }
     }
