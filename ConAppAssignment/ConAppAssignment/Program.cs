d69f7b5c46d6b93c238dc9694513ba347ee52b13
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2021-12-16 14:55:35 -0800

    Console App Assignment
    Created an Application.

---
+++ ConAppAssignment/ConAppAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] artPaints = { "Oil: ", "Acrylic: ", "Pastel: ", "Ink Wash: " };
+            Console.WriteLine("Please type something to add to the end: ");
+            string myPaints = Convert.ToString(Console.ReadLine());
+            for (int p = 0; p < artPaints.Length; p++)
+            {
+                Console.WriteLine(artPaints[p] + myPaints);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
