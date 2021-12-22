3deb254599b0ebf5306f6aa1f87bc2969dcbc7de
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2021-12-22 12:11:42 -0800

    Calling Methods
    Takes a class created in a separate .cs file and utilizes its methods for 3 different math operations.

---
+++ CallingMethods/CallingMethods/Hierarchy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Hierarchy
+    {
+        public int Method1(int userInput)
+        {
+            return userInput + 15;
+        }
+        public int Method2(int userInput)
+        {
+            return userInput - 2;
+        }
+        public int Method3(int userInput)
+        {
+            return userInput * 4;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hierarchy math = new Hierarchy();
+            
+            Console.WriteLine("What number would you like to use math operations on?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(math.Method1(userInput));
+
+            Console.WriteLine(math.Method2(userInput));
+
+            Console.WriteLine(math.Method3(userInput));
+
+            Console.ReadLine();
+        }
+    }
+}
