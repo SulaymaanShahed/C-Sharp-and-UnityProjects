e31fd556e5ef5cdd7fc3ebd295ebce6c356efa0a
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2021-12-23 16:46:49 -0800

    Method Assignment
    In this I have 3 methods each containing a different math operation.

---
+++ MainMethod/MainMethod/Operations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Operations
+    {
+        public int Method1(int userInput)
+        {
+            return userInput - 8;
+        }
+        public decimal Method2(decimal userDec)
+        {
+            int conum = Convert.ToInt32(userDec);
+            return conum * 50;
+        }
+        public string Method3(string userStr)
+        {
+            int coStr = Convert.ToInt32(userStr);
+            return coStr * 6;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations math = new Operations();
+
+            Console.WriteLine("Please input a number for subtraction.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(math.Method1(userInput));
+
+            Console.WriteLine("Please input a number to multiply.");
+            decimal userDec = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine(math.Method2(userDec));
+
+            Console.WriteLine("Please type a number in string.");
+            string userStr = Convert.ToString(Console.ReadLine());
+     
+            Console.WriteLine(math.Method3(userStr));
+
+            Console.ReadLine();
+        }
+    }
+}
