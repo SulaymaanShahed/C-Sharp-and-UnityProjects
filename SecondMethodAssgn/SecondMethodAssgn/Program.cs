5cc1ea954a0b318e17401d66e38004bd74577752
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2021-12-26 20:59:23 -0800

    Second Assignment
    Method assignment doing math operation.

---
+++ SecondMethodAssgn/SecondMethodAssgn/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondMethodAssgn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sMethod math = new sMethod();
+
+            Console.WriteLine("Choose a number to multiply");
+            int userInt = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(math.Method1(userInt));
+            Console.ReadLine();
+
+            Console.WriteLine("Optional Number: Input a 2nd number");
+            int secInt = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ SecondMethodAssgn/SecondMethodAssgn/sMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondMethodAssgn
+{
+    class sMethod
+    {
+        public int Method1(int userInt)
+        {
+            int num1 = 4;
+            return num1 * userInt;
+        }
+
+    }
+}
