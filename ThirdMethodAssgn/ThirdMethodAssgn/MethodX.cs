fb2d064d9a44d4d6f73d01b52d8b07b9df2438d0
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2021-12-27 18:46:33 -0800

    Third Method Assignment
    Created class that does math operation on one integer and displays another.

---
+++ ThirdMethodAssgn/ThirdMethodAssgn/MethodX.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdMethodAssgn
+{
+    class MethodX
+    {
+        public void MethodOne(int num1, int num2)
+        {
+            int result = num1 + 15;
+            Console.WriteLine("Here is the second number " + num2);
+        }
+    }
+}
---
+++ ThirdMethodAssgn/ThirdMethodAssgn/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdMethodAssgn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodX name = new MethodX();
+
+            name.MethodOne(4, 7);
+            name.MethodOne(num1: 4, num2: 6);
+            Console.ReadLine();
+        }
+    }
+}
