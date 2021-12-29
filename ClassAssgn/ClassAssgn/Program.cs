7cba7e92237baa4dbd77c640f1db51567f6925e9
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2021-12-28 18:35:43 -0800

    Class Assignment
    Class Assignment with lots of method actions.

---
+++ ClassAssgn/ClassAssgn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssgn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            myMethod myMath = new myMethod();// Instantiated class in Main()
+
+            Console.WriteLine("Enter a number to divide by 2 "); //Requested users integer for operation
+            int userInput = Convert.ToInt32(Console.ReadLine()); //Users input held in int "userInput"
+            myMath.myMethods (userInput); //Method called on input integer.
+
+            int secInt = 40; 
+            myMethod.myMethods (out secInt);
+            Console.WriteLine(secInt);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassAssgn/ClassAssgn/myMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssgn
+{
+    public class myMethod
+    {
+        public void myMethods(int firstInt)
+        {
+            int result = firstInt / 2;
+            Console.WriteLine(result);
+        }
+        public static void myMethods(out int secInt)
+        {
+            secInt = 10;
+        }
+    }
+}
