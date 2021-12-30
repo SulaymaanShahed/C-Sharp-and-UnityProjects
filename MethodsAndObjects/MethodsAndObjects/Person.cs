31c867d76a850c6b6fe6aa0f692270a1ef7b588f
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2021-12-29 18:58:26 -0800

    Assignment
    Methods and Objects Assignment.

---
+++ MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Person
+    {
+        private string _firstname = "Sample";
+        private string _lastname = "Student";
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + _firstname + _lastname);
+        }
+
+        
+    }
+}
---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee name = new Employee();
+            name.FirstName =
+            name.LastName =
+            name.SayName();
+            Console.ReadLine();
+        }
+    }
+}
