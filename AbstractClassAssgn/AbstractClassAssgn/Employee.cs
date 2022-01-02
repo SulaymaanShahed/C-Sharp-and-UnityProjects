5a51ceb09b2f8477d5c652404a544f221f0b29c7
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2022-01-02 14:36:09 -0800

    Update
    Updated code for another assignment.

--- AbstractClassAssgn/AbstractClassAssgn/Employee.cs
+++ AbstractClassAssgn/AbstractClassAssgn/Employee.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace AbstractClassAssgn
 {
-    public class Employee : Person
+    public class Employee : Person, IQuittable
     {
         public override void SayName()
         {
             Console.WriteLine("Full Name: " + FirstName + LastName);
         }
+
+        IQuittable obj1 = new Employee();
+
     }
 }
