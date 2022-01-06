3c47d06eb73c03697c909170b49ae4ebf732a74e
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2022-01-05 17:29:57 -0800

    Operators assignment.
    Quick equality operator usage here.

--- AbstractClassAssgn/AbstractClassAssgn/Employee.cs
+++ AbstractClassAssgn/AbstractClassAssgn/Employee.cs
@@ -17,6 +17,5 @@ namespace AbstractClassAssgn
         {
             Console.WriteLine("You quit!");
         }
-
     }
 }
---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    public class Employee
+    {
+       public int Id { get; set; }
+
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+    }
+}
