d780d6e6ddd203cf71d24f233474c726fa9f8ff0
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2022-01-06 16:03:07 -0800

    Parameters Assignment.
    Created a Console Application that takes different parameters.

---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+
+            List<string> Things = new List<string>
+            { "Tape", "Pencil", "Marker", "Folder" };
+
+            Employee<int> employeeInt = new Employee<int>();
+
+            List<int> Intthings = new List<int>
+            { 4, 6, 9, 15, 22, 37 };
+        }
+    }
+}
