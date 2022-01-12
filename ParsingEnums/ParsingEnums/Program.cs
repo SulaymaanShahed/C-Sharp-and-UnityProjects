a22e92bb363e9692317edcc96a1a9ef775f3df15
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2022-01-11 20:14:44 -0800

    Parsing Enums Challenge
    Assignment with enums being parsed for Days in a week.

---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnums
+{
+    public class Program
+    {   
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week.");
+
+            string input1 = Console.ReadLine();
+            try 
+            {
+                DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), input1);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please enter an actual day of the week.", e);
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
