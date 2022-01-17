7392e6a05506ac57a65c29ebdf72c2b910f74e49
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2022-01-17 15:13:00 -0800

    Lambda Expressions
    Quick Lambda Assignment.

--- LambdaExpressions/LambdaExpressions/Program.cs
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -10,6 +10,7 @@ namespace LambdaExpressions
     {
         static void Main(string[] args)
         {
+            /* Start list of employees */
             List<Employees> employList = new List<Employees>();
 
             employList.Add(new Employees()
@@ -52,11 +53,11 @@ namespace LambdaExpressions
             {
                 Firstname = "Bellam", Lastname = "Iver", Id = 10
             });
+            /* End list of employees */
 
-            Console.WriteLine(employList);
+            List<Employees> joesList = new List<Employees>(); //List made for employees with firstname "Joe"
 
-            List<Employees> joesList = new List<Employees>();
-
+            /* Start of foreach loop that iterates through list of employees */
             foreach (Employees employees in employList)
             {
                 if (employees.Firstname == "Joe")
@@ -64,7 +65,43 @@ namespace LambdaExpressions
                     joesList.Add(employees);
                 }
             }
-            Console.WriteLine(joesList);
+            /* End of foreach loop */
+
+            /* Start of foreach loop for employees with the firstname "Joe" */
+            foreach (Employees joes in joesList)
+            {
+                Console.WriteLine(joes.Firstname + joes.Lastname);
+            }
+            /* End of foreach loop */
+
+            /* Start new employee list for lambda expression */
+            List<Employees> ajoeList = new List<Employees>() { 
+                new Employees{ Firstname = "Bart", Lastname = "Jensen", Id =  1, },
+                    new Employees{ Firstname = "Kelly", Lastname = "Haddes", Id =  2, },
+                    new Employees{ Firstname = "Joe", Lastname = "Machio", Id =  3, },
+                    new Employees{ Firstname = "Tera", Lastname = "Corsinth", Id =  4, },
+                    new Employees{ Firstname = "Qorin", Lastname = "Epherin", Id =  5, },
+                    new Employees{ Firstname = "Gareck", Lastname = "Lortimar", Id =  6, },
+                    new Employees{ Firstname = "Solara", Lastname = "Yjaeger", Id =  7, },
+                    new Employees{ Firstname = "Yassir", Lastname = "Puelora", Id =  8, },
+                    new Employees{ Firstname = "Joe", Lastname = "Ocampo", Id =  9, },
+                    new Employees{ Firstname = "Ingrid", Lastname = "Xanthus", Id =  10, },
+            }; //End new employee list
+
+            var lamJoes = ajoeList.Where(joe => joe.Firstname == "Joe").ToList(); //Lambda expression
+
+            foreach (var joe in lamJoes) //Foreach loop to display all employees with firstname "Joe"
+            {
+                Console.WriteLine(joe.Firstname + joe.Lastname);
+            }
+
+            List<Employees> empId = ajoeList.Where(employId => employId.Id > 5).ToList();
+
+            foreach (Employees employee in empId)
+            {
+                Console.WriteLine(employee.Firstname + employee.Lastname + " Id:" + employee.Id);
+            }
+
             Console.ReadLine();
         }
     }
