12269374d06138bc9727e0d05aa86c12875e67f5
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2022-01-20 19:08:16 -0800

    Assignment
    Date and Time Assignment.

---
+++ InputChall/InputChall/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputChall
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            using (StreamWriter intFile = new StreamWriter(@"C:\Users\sulay\ansel\log.txt", true))
+            {
+                intFile.WriteLine(DateTime.Now);
+                intFile.WriteLine(userInput);
+            }
+            Console.ReadLine();
+        }
+    }
+}
