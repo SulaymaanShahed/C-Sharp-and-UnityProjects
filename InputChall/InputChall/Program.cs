259500aff29c53d7632a73d5939ef44cf91b6c7f
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2022-01-21 13:29:45 -0800

    Update
    Fixed code to display initial date and time, then adds user input and displays new date and time.

--- InputChall/InputChall/Program.cs
+++ InputChall/InputChall/Program.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace InputChall
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
+            DateTime lDate = DateTime.Now;
+
             Console.WriteLine("Please input a number");
             int userInput = Convert.ToInt32(Console.ReadLine());
 
-            using (StreamWriter intFile = new StreamWriter(@"C:\Users\sulay\ansel\log.txt", true))
-            {
-                intFile.WriteLine(DateTime.Now);
-                intFile.WriteLine(userInput);
-            }
+            DateTime mDate = lDate.AddHours(userInput);
+           
+            Console.WriteLine("Initial DateTime = {0:dd} {0:y}, {0:t}", lDate);
+            Console.WriteLine("\nNew DateTime = {0:dd} {0:y}, {0:t} ", mDate);
             Console.ReadLine();
         }
     }
