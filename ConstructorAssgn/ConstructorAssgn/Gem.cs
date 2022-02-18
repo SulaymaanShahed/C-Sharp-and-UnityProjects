c04f5042173e33e6bdd2ee8b50c77e6b7d64b475
SulaymaanShahed <sulaymaanyshahed@gmail.com>
2022-02-17 17:22:53 -0800

    Constructors Assignment
    Utilizing constructors in a Console App.

---
+++ ConstructorAssgn/ConstructorAssgn/Gem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssgn
+{
+    public class Gem
+    {
+        public Carat Carat { get; set; }
+        public Shape Shape { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} of {1}", Carat, Shape);
+        }
+    }
+    public enum Carat
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five
+    }
+    public enum Shape
+    {
+        Round,
+        Princess,
+        Marquise,
+        Emerald,
+        Pear,
+        Oval,
+        Heart
+    }
+}
---
+++ ConstructorAssgn/ConstructorAssgn/Shards.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssgn
+{
+    class Shards
+    {
+        public Shards(string name) : this(name, 100)
+        {
+        }
+
+        public Shards(string name, int amount)
+        {
+            Type = new List<Gem>();
+            Name = name;
+
+        }
+        public List<Gem> _type = new List<Gem>();
+        public List<Gem> Type { get { return _type; } set { _type = value; } }
+        public string Name { get; }
+        public int V { get; set; }
+    }
+}
